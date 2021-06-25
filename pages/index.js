c69a903c383a21536ed48f25b9e681882028cd90
tanzilansari <56162557+tanzilansari@users.noreply.github.com>
2021-06-25 17:22:57 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! Tanzil Ansariiiiiii</a>
+          Welcome to <a href="https://nextjs.org">Next.js! Tanzil Ansariiiiiii hello</a>
         </h1>
 
         <p className={styles.description}>
